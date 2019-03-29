b3d337e12c5233239fc7fad69f720c4a1fe6e80d
Lucas A <game4allyt@gmail.com>
2019-03-29 19:14:28 +0100

    Added GetUsersAsync overloads

--- circles.NET/CirclesAPIClient.cs
+++ circles.NET/CirclesAPIClient.cs
@@ -1,6 +1,10 @@
-﻿using circles.NET.Exceptions;
+﻿using circles.NET.Enums;
+using circles.NET.Exceptions;
 using circles.NET.HTTP;
+using circles.NET.Models;
+using System;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace circles.NET
 {
@@ -14,9 +18,15 @@ namespace circles.NET
         /// </summary>
         public CirclesHttpClient Client { get; }
 
-        public CirclesAPIClient()
+        private CirclesAPIClient()
         {
             Client = new CirclesHttpClient(this);
+        }
+
+        public CirclesAPIClient(string apiKey)
+            : this()
+        {
+            APIKey = apiKey;
         }
 
         /// <summary>
@@ -35,27 +45,46 @@ namespace circles.NET
 
         protected string ApiKey;
 
+        //GetUsersAsync
+
+        public async Task<APIUser[]> GetUsersAsync(string name) => await Client.GetFromJSON<APIUser[]>(CreateURL("get_user", "k", APIKey, "u", name));
+
+        public async Task<APIUser[]> GetUsersAsync(int id) => await Client.GetFromJSON<APIUser[]>(CreateURL("get_user", "k", APIKey, "u", id, "type", "id"));
+
+        public async Task<APIUser[]> GetUsersAsync(string name, Gamemode mode) => await Client.GetFromJSON<APIUser[]>(CreateURL("get_user", "k", APIKey, "u", name, "m", (int)mode));
+
+        public async Task<APIUser[]> GetUsersAsync(int id, Gamemode mode) => await Client.GetFromJSON<APIUser[]>(CreateURL("get_user", "k", APIKey, "u", id, "m", (int)mode, "type", "id"));
+
         /// <summary>
         /// The defaut address to which API requests are made.
         /// <para>Override this if you need to pass API requests to eg: Ripple API</para>
         /// </summary>
         protected virtual string BaseEndpoint => $"https://osu.ppy.sh/api/";
 
-        protected string CreateURL(string endpoint, params object[] parameters)
+        /// <summary>
+        /// Returns a complete URL to the API
+        /// </summary>
+        /// <param name="endpoint">The endpoint</param>
+        /// <param name="queryStrings">The query strings</param>
+        /// <returns></returns>
+        protected string CreateURL(string endpoint, params object[] queryStrings)
         {
             var sb = new StringBuilder();
+            sb.Append(BaseEndpoint);
+            sb.Append(endpoint);
 
-            for (int i = 0; i < parameters.Length; i += 2) //query strings are given this way [0] = QueryStringName, [1] = QueryStringValue
+            for (int i = 0; i < queryStrings.Length; i += 2) //query strings are given this way [0] = QueryStringName, [1] = QueryStringValue
             {
-                if (i == 0)
+                if (i == 0) //first parameter should always be API key
                 {
-                    sb.Append($"?{parameters[i].ToString()}={parameters[i + 1].ToString()}");
+                    sb.Append($"?{queryStrings[i].ToString()}={queryStrings[i + 1].ToString()}");
                     continue;
                 }
 
-                sb.Append($"&{parameters[i].ToString()}={parameters[i + 1].ToString()}");
+                if (queryStrings[i + 1] != null) //if the query string value is != null , let's add it to the url
+                    sb.Append($"&{queryStrings[i].ToString()}={queryStrings[i + 1].ToString()}");
             }
-
+            Console.WriteLine(sb.ToString());
             return sb.ToString();
         }
     }
---
+++ circles.NET/Enums/Gamemode.cs
@@ -0,0 +1,10 @@
+﻿namespace circles.NET.Enums
+{
+    public enum Gamemode
+    {
+        Standard = 0,
+        Taiko = 1,
+        Catch = 2,
+        Mania = 3
+    }
+}
