153f32247bbeaa87943f73069503e9c5f1cc448c
Lucas A <game4allyt@gmail.com>
2019-03-29 22:03:54 +0100

    Added APIReplay

---
+++ circles.NET/Models/APIReplay.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace circles.NET.Models
+{
+    public sealed class APIReplay
+    {
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+    }
+}
