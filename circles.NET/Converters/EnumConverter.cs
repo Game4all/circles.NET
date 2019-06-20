89656550255256b55badc6f2566851e7ffd2052d
Lucas A <game4allyt@gmail.com>
2019-06-20 20:24:05 +0200

    Create EnumConverter converting any enum type from JSON

---
+++ circles.NET/Converters/EnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace circles.NET.Converters
+{
+    public class EnumConverter<T> : JsonConverter
+        where T : Enum
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(T);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var val = int.Parse((string)reader.Value);
+            var enumVal = Enum.ToObject(typeof(T), val);
+            return enumVal;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
