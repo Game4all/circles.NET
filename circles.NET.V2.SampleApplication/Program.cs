01b7255ee8f4080ba1f1056dd470cc1c17f680b0
Lucas A <game4allyt@gmail.com>
2020-01-26 13:03:11 +0100

    Add SampleApplication for APIv2 client

---
+++ circles.NET.V2.SampleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿//
+// This SampleApplication demonstrates how to use circles.NET to fetch the top 50 scores of the specified user using the APIv2 client.
+//
+
+using System;
+
+namespace circles.NET.V2.SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
